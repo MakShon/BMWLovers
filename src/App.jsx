e3f2bf6422fc981daf2e060935e2f86cbb44b12c
MakShon <maksym.baran69@gmail.com>
2024-10-23 16:25:45 +0300

    feature: chaged condig

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from 'react';
 
-import styles from './App.css';
-
-const App = () =>  {
-    return (
-        <div>Hello</div>
-    );
-}
-
+const App = () => {
+  return <div>Hello</div>;
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import * ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-
-import App from './App'; // require('./App').default
+import './assets/index.css';
+import App from './App';
 
 const containerElement = document.querySelector('#root');
 
